0220b265b9870df7b5a2a4e60f583291612facdc
Vlad Balas <balash_vlad@yahoo.com>
2023-02-23 21:59:40 +0000

    Created the Navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Navigation />
+        {/*<Logo />
+        <ImageLinkForm />
+        <FaceRecognition />*/}
       </div>
     );
   }
